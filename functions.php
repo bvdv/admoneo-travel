d43c91d87f4ac8224ddcaff0aadd6e77766791b2
bvdv <bvdv2019@gmail.com>
2018-03-13 16:16:51 +0200

    Custom scripts

--- functions.php
+++ functions.php
@@ -146,6 +146,7 @@ function admoneo_travel_scripts() {
 
 	wp_enqueue_script( 'admoneo-travel-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
     
+    //custom scripts
     wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '3.3.7', true);
 
     wp_enqueue_script( 'owl-carousel-js', get_template_directory_uri() . '/assets/js/owl.carousel.min.js', array('jquery'), '2.2.1', true);
