7e0c8f6e1a3a5572e6045e1e79c11d88e1d1842c
bvdv <bvdv2019@gmail.com>
2018-03-13 16:15:30 +0200

    Custom scripts

--- functions.php
+++ functions.php
@@ -140,11 +140,23 @@ function admoneo_travel_scripts() {
 	//maybe be not need
 	wp_enqueue_style( 'contact-css', get_template_directory_uri() . '/assets/css/contact.css');
 
-	//custom scripts
+	
 
 	wp_enqueue_script( 'admoneo-travel-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'admoneo-travel-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '3.3.7', true);
+
+    wp_enqueue_script( 'owl-carousel-js', get_template_directory_uri() . '/assets/js/owl.carousel.min.js', array('jquery'), '2.2.1', true);
+    
+    //need ot not???
+	wp_enqueue_script( 'contacts-js', get_template_directory_uri() . '/assets/js/contact.js', array('jquery'), '20151215', true);
+
+    wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/assets/js/jquery.flexslider.js', array('jquery'), '2.7.0', true);
+
+    wp_enqueue_script( 'cystom-scripts', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '20151215', true);
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
